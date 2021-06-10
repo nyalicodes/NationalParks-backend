12b87264704964efb03e38a09e7213d9722646b2
Nyaladzi Trust Tjibha <43335962+nyalicodes@users.noreply.github.com>
2021-06-10 09:23:32 +0200

    Created NationalPark Dto

---
+++ src/ShaCompanyName.ShaProjectName/Services/NationalParks/DTO/NationalParkDto.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShaCompanyName.ShaProjectName.Services.NationalParks.DTO
+{
+    public class NationalParkDto: EntityDto<Guid>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Established { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+        public string Suburb { get; set; }
+        public string Town { get; set; }
+        public string POBox { get; set; }
+        public string FullAddress { get; set; }
+    }
+}
