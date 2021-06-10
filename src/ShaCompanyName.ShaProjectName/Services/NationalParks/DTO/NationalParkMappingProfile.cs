43cb7a6e02edb7be1b8589975b82804eef0ad06f
Nyaladzi Trust Tjibha <43335962+nyalicodes@users.noreply.github.com>
2021-06-10 11:57:20 +0200

    AutoMapper Configuration

---
+++ src/ShaCompanyName.ShaProjectName/Services/NationalParks/DTO/NationalParkMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ShaCompanyName.ShaProjectName.Domain;
+using ShaCompanyName.ShaProjectName.Services.NationalParks.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShaCompanyName.ShaProjectName.Services.NationalParks
+{
+    public class NationalParkMappingProfile : Profile
+    {
+        public NationalParkMappingProfile()
+        {
+            CreateMap<NationalPark, NationalParkDto>().ReverseMap();
+        }
+    }
+}
